970a36ae0c6b1561bf6cd8a96f0a4763d062ed49
Frantisek Burian <BuFran@seznam.cz>
2018-12-08 13:28:13 +0100

    Taking some shots of plums with guardians - being in christmas happiness on second computer.
      - Need to plant more plums, supplies of shots are rapidly decreasing ...

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -3,116 +3,78 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
-namespace Day4
+namespace AdventOfCode
 {
     class Program
     {
-        struct cmd
+        class Row
         {
-            public DateTime p1;
-            public string p2;
-        }
+            public DateTime d;
+            public int id;
 
-        class guard
-        {
-            public int id = 0;
-            public DateTime sleep;
-            public DateTime wake;
-            public double length { get { return (wake - sleep).TotalMinutes; } }
-
-            public IEnumerable<int> Mins
+            public Row(string s)
             {
-                get
-                {
-                    for (int sta = sleep.Minute; sta < wake.Minute; sta++)
-                        yield return sta;
-                }
+                var ss = s.Split('[',']', '#');
+                d = DateTime.Parse(ss[1]);
+
+                if (s.Contains("falls"))
+                    id = -1;
+                else if (s.Contains("wakes"))
+                    id = -2;
+                else
+                    id = int.Parse(ss[3].Split(' ')[0]);
             }
         }
+
         static void Main(string[] args)
         {
-            var p = File.ReadAllLines("input").Select(Transform).OrderBy(x=>x.p1).ToArray();
-           // var p = File.ReadAllLines("TextFile1.txt").Select(Transform).OrderBy(x => x.p1).ToArray();
+            var data = File.ReadAllLines("input").Select(x => new Row(x)).OrderBy(x => x.d).ToArray();
 
-            var l = new List<guard>();
+            var table = new Dictionary<int,Dictionary<int,int>>();
 
-            guard actual = null;
-
-            for (int i=0;i<p.Length;i++)
-            {
-                if (p[i].p2.Contains("Guard"))
+            int current = 0;
+            int start = 0;
+            foreach (var row in data)
+                switch (row.id)
                 {
-                    string[] pp = p[i].p2.Split(' ', '#');
-                    
-                    actual = new guard() { id = int.Parse(pp[3]) };
-                    l.Add(actual);
-                }
-                else if (p[i].p2.Contains("falls"))
-                {
-                    Debug.Assert(actual != null);
-                    actual.sleep = p[i].p1;
-                }
-                else if (p[i].p2.Contains("wakes"))
-                {
-                    Debug.Assert(actual != null);
-                    actual.wake = p[i].p1;
-                    actual = new guard() { id = actual.id };
-                    l.Add(actual);
-                }
-            }
+                    default:
+                        current = row.id;
+                        break;
 
-            var grp = l.GroupBy(x => x.id).OrderByDescending(x => x.Sum(y=> y.length)).First();
+                    case -1:
+                        start = row.d.Minute;
+                        break;
 
-            Dictionary<int, int> mins = new Dictionary<int, int>();
+                    case -2:
+                        Dictionary<int,int> mins;
 
-            for (int i=0;i<60;i++)
-                mins.Add(i, 0);
+                        if (!table.TryGetValue(current, out mins))
+                        {
+                            mins = new Dictionary<int, int>();
+                            for (int i = 0 ; i < 60 ; i++)
+                                mins[i] = 0;
 
-            foreach (var u in grp)
-            {
-                foreach (var min in u.Mins)
-                    mins[min]++;
-            }
+                            table.Add(current, mins);
+                        }
 
-            var m = mins.OrderByDescending(x => x.Value).First().Key;
-
-            Console.WriteLine("Guard #{0} sleeps on {1} minute {2}", grp.Key, m, grp.Key * m);
-
-            var grp3 = l.GroupBy(x => x.id).ToArray();
-
-            Dictionary<int, Dictionary<int, int>> times = new Dictionary<int, Dictionary<int, int>>();
-
-            foreach (var x in l)
-            {
-                Dictionary<int, int> v;
-                if (!times.TryGetValue(x.id, out v))
-                {
-                    v = new Dictionary<int, int>();
-
-                    for (int i = 0; i < 60; i++)
-                        v.Add(i, 0);
-
-                    times.Add(x.id, v);
+                        for (int min = start ; min < row.d.Minute ; min++)
+                            mins[min]++;
+                        break;
                 }
 
-                foreach (var min in x.Mins)
-                    v[min]++;                
-            }
+            var sleepy = table.OrderByDescending(x=>x.Value.Sum(y=> y.Value)).First();
+            var minute = sleepy.Value.OrderByDescending(x=>x.Value).First().Key;
 
-            var ord = times.OrderByDescending(x => x.Value.Values.Max()).ToArray();
-        }
+            Debug.WriteLine("Key {0} on minute {1} hash {2}", sleepy.Key, minute, sleepy.Key * minute);
 
-        static cmd Transform(string s)
-        {
-            string[] strs = s.Split('[', ']');
-            cmd result = new cmd();
-            result.p1 = DateTime.Parse(strs[1]);
-            result.p2 = strs[2];
-            return result;
+
+            var psa = table.OrderByDescending(x => x.Value.Max(y => y.Value)).First();
+            var minutea = psa.Value.OrderByDescending(x=>x.Value).First().Key;
+
+            Debug.WriteLine("Key {0} on minute {1} hash {2}", psa.Key, minutea, psa.Key * minutea);
+
+
         }
     }
 }
