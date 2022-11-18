db3f4728b539dd63ae380877e2f55faef522fed9
Frantisek Burian <BuFran@seznam.cz>
2022-11-18 20:43:01 +0100

    Prepared all days for solution

---
+++ 2020/day03.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  ifstream f("day03.test");
+
+  if (!f.is_open())
+    return -1;
+
+  while (!f.eof()) {
+    int d;
+    f >> d;
+  }
+
+  return 0;
+}
