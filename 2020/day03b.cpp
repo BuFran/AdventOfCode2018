635af8a84db1257f141f128035fba597537d0b27
Frantisek Burian <BuFran@seznam.cz>
2022-11-18 21:25:01 +0100

    Day03b end solving

--- 2020/day03b.cpp
+++ 2020/day03b.cpp
@@ -23,13 +23,18 @@ int main()
     land.push_back(vc);
   }
 
-  size_t n = 0;
   size_t z = land[0].size();
 
-  for (int row = 1, col = 3; row < land.size(); ++row, col = (col + 3) % z)
-    if (land[row][col] == '#')
-      n++;
+  int rg[] = {1, 3, 5, 7, 1};
+  int dw[] = {1, 1, 1, 1, 2};
+  size_t res[] = {0,0,0,0, 0};
 
-  cout << n;
+  for (int i=0;i<5;++i) {
+    for (size_t row = dw[i], col = rg[i]; row < land.size(); row += dw[i], col = (col + rg[i]) % z)
+      if (land[row][col] == '#')
+        res[i]++;
+  }
+
+  cout << res[0] << " " << res[1] << " " << res[2] << " " << res[3] << " " << res[4] << " " << res[0] * res[1] * res[2] * res[3] * res[4];
   return 0;
 }
