f2888a9808d42b69fad0c1aec1407e296449d41b
Frantisek Burian <BuFran@seznam.cz>
2022-11-18 20:58:51 +0100

    Day02 solved

--- 2020/day02.cpp
+++ 2020/day02.cpp
@@ -4,15 +4,24 @@ using namespace std;
 
 int main()
 {
-  ifstream f("day02.test");
+  ifstream f("day02.dat");
 
   if (!f.is_open())
     return -1;
 
+  int c = 0;
   while (!f.eof()) {
-    int d;
-    f >> d;
+    int min, max;
+    char letter, dot;
+    string pwd;
+    f >> min >> max >> letter >> dot >> pwd;
+
+    size_t n = count(pwd.begin(),pwd.end(), letter);
+
+    if ((n >= min) && (n <= -max))
+      c++;
   }
 
+  cout << c;
   return 0;
 }
